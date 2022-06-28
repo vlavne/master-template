d0bb2a4367fdd1b66f21bd44a8cd5f5c821f0d3d
ZyXeLinua <nev-dima@yandex.ru>
2022-06-28 16:29:58 +0300

    updated bulk installed / enabled plugins list

--- inc/tgm_activation.php
+++ inc/tgm_activation.php
@@ -33,7 +33,7 @@
  */
 require_once get_template_directory() . '/inc/class-tgm-plugin-activation.php';
 
-add_action( 'tgmpa_register', 'master_template_register_required_plugins' );
+add_action('tgmpa_register', 'master_template_register_required_plugins');
 
 /**
  * Register the required plugins for this theme.
@@ -52,7 +52,8 @@ add_action( 'tgmpa_register', 'master_template_register_required_plugins' );
  *
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
-function master_template_register_required_plugins() {
+function master_template_register_required_plugins()
+{
     /*
      * Array of plugin arrays. Required keys are name and slug.
      * If the source is NOT from the .org repo, then source is also required.
@@ -61,65 +62,72 @@ function master_template_register_required_plugins() {
 
         // This is an example of how to include a plugin bundled with a theme.
         array(
-            'name'               => 'Advanced Custom Fields PRO', // The plugin name.
-            'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
-            'source'             => get_template_directory() . '/lib/plugins/advanced-custom-fields-pro.zip', // The plugin source.
-            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-            'version'            => '5.12.2', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+            'name' => 'Advanced Custom Fields PRO', // The plugin name.
+            'slug' => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
+            'source' => get_template_directory() . '/lib/plugins/advanced-custom-fields-pro.zip', // The plugin source.
+            'required' => true, // If false, the plugin is only 'recommended' instead of required.
+            'version' => '5.12.2', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+            'force_activation' => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
             'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+            'external_url' => '', // If set, overrides default API URL and points to an external URL.
+            'is_callable' => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
         ),
         array(
-            'name'      => 'Classic Editor',
-            'slug'      => 'classic-editor',
-            'required'  => true,
-            'force_activation'   => true,
+            'name' => 'Classic Editor',
+            'slug' => 'classic-editor',
+            'required' => true,
+            'force_activation' => true,
+            'is_automatic' => true,
         ),
         array(
-            'name'      => 'Safe SVG',
-            'slug'      => 'safe-svg',
-            'required'  => false,
-            'force_activation'   => true,
+            'name' => 'Safe SVG',
+            'slug' => 'safe-svg',
+            'required' => false,
+            'force_activation' => true,
+            'is_automatic' => true,
         ),
         array(
-            'name'      => 'Contact Form 7',
-            'slug'      => 'contact-form-7',
-            'required'  => true,
-            'force_activation'   => true,
+            'name' => 'Contact Form 7',
+            'slug' => 'contact-form-7',
+            'required' => true,
+            'force_activation' => true,
+            'is_automatic' => true,
         ),
         array(
-            'name'      => 'Yoast Duplicate Post',
-            'slug'      => 'duplicate-post',
-            'required'  => false,
+            'name' => 'Yoast Duplicate Post',
+            'slug' => 'duplicate-post',
+            'required' => false,
         ),
         array(
-            'name'      => 'Yoast SEO',
-            'slug'      => 'wordpress-seo',
-            'required'  => false,
+            'name' => 'Yoast SEO',
+            'slug' => 'wordpress-seo',
+            'required' => false,
         ),
         array(
-            'name'      => 'Smush',
-            'slug'      => 'wp-smushit',
-            'required'  => false,
+            'name' => 'Smush',
+            'slug' => 'wp-smushit',
+            'required' => false,
         ),
         array(
-            'name'      => 'Flamingo',
-            'slug'      => 'flamingo',
-            'required'  => false,
+            'name' => 'Flamingo',
+            'slug' => 'flamingo',
+            'required' => false,
         ),
         array(
-            'name'      => 'All-in-One WP Migration',
-            'slug'      => 'all-in-one-wp-migration',
-            'required'  => false,
+            'name' => 'All-in-One WP Migration',
+            'slug' => 'all-in-one-wp-migration',
+            'required' => false,
+            'force_activation' => true,
+            'is_automatic' => true,
         ),
         array(
-            'name'               => 'All-in-One WP Migration Unlimited Extension',
-            'slug'               => 'all-in-one-wp-migration-unlimited-extension',
-            'source'             => get_template_directory() . '/lib/plugins/all-in-one-wp-migration-unlimited-extension.zip',
-            'required'           => false,
-            'version'            => '2.31',
+            'name' => 'All-in-One WP Migration Unlimited Extension',
+            'slug' => 'all-in-one-wp-migration-unlimited-extension',
+            'source' => get_template_directory() . '/lib/plugins/all-in-one-wp-migration-unlimited-extension.zip',
+            'required' => false,
+            'version' => '2.31',
+            'force_activation' => true,
+            'is_automatic' => true,
         ),
 
         // This is an example of the use of 'is_callable' functionality. A user could - for instance -
@@ -146,16 +154,16 @@ function master_template_register_required_plugins() {
      * Only uncomment the strings in the config array if you want to customize the strings.
      */
     $config = array(
-        'id'           => 'master-template',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+        'id' => 'master-template',                 // Unique ID for hashing notices for multiple instances of TGMPA.
         'default_path' => '',                      // Default absolute path to bundled plugins.
-        'menu'         => 'tgmpa-install-plugins', // Menu slug.
-        'parent_slug'  => 'themes.php',            // Parent menu slug.
-        'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-        'has_notices'  => true,                    // Show admin notices or not.
-        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-        'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+        'menu' => 'tgmpa-install-plugins', // Menu slug.
+        'parent_slug' => 'themes.php',            // Parent menu slug.
+        'capability' => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+        'has_notices' => true,                    // Show admin notices or not.
+        'dismissable' => true,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg' => '',                      // If 'dismissable' is false, this message will be output at top of nag.
         'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-        'message'      => '',                      // Message to output right before the plugins table.
+        'message' => '',                      // Message to output right before the plugins table.
 
         /*
         'strings'      => array(
@@ -235,5 +243,5 @@ function master_template_register_required_plugins() {
         */
     );
 
-    tgmpa( $plugins, $config );
+    tgmpa($plugins, $config);
 }
